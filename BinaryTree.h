040fd0407631beb99f509b9158426cfe2863b4ac
TGG <info@thegorjgroup.com>
2022-11-07 12:01:35 +0000

    Added constructors for Book and Order objects

--- BinaryTree.h
+++ BinaryTree.h
@@ -5,32 +5,45 @@ class LinkedBinaryTree {
 
     protected:
     
-    struct Limit;
+        struct Limit;
 
-    struct Order {
-        int idNumber;
-        bool buyOrSell;
-        int shares;
-        int entryTime;
-        int eventTime;
-        Order* nextOrder;
-        Order* prevOrder;
-        Limit* parentLimit;
-    };
+        struct Order {
+            int idNumber;
+            bool buyOrSell;
+            int shares;
+            int entryTime;
+            int eventTime;
+            Order* nextOrder;
+            Order* prevOrder;
+            Limit* parentLimit;
 
-    struct Limit {
-        int limitPrice;
-        int size;
-        int totalVolume;
-        Limit *parent;
-        Limit *leftChild;
-        Limit *rightChild;
-        Order *headOrder;
-        Order *tailOrder;
+            Order() : idNumber(), buyOrSell(), shares(), entryTime(), eventTime(), nextOrder(NULL),
+                prevOrder(NULL), parentLimit(NULL) { } 
+        };
 
-        Limit() : limitPrice(), size(), totalVolume(), parent(NULL), 
-                leftChild(NULL), rightChild(NULL), headOrder(NULL), tailOrder(NULL) { } // Constructor  
-    };
+        struct Limit {
+            int limitPrice;
+            int size;
+            int totalVolume;
+            Limit *parent;
+            Limit *leftChild;
+            Limit *rightChild;
+            Order *headOrder;
+            Order *tailOrder;
+
+            Limit() : limitPrice(), size(), totalVolume(), parent(NULL), 
+                    leftChild(NULL), rightChild(NULL), headOrder(NULL), tailOrder(NULL) { } // Constructor  
+        };
+
+
+        struct Book {
+            Limit *buyTree;
+            Limit *sellTree;
+            Limit *lowestSell;
+            Limit *highestBuy;
+
+            Book() : buyTree(NULL), sellTree(NULL), lowestSell(NULL), highestBuy(NULL) { }
+        };
 
     
     class Position {
