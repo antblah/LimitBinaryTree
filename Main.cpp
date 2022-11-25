36a1078096b8d231d79f6665a4db42b79fffb481
TGG <info@thegorjgroup.com>
2022-11-25 15:49:09 +0000

    Changed addOrderFront

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,34 @@
 int main() {
 
     BinaryTree *bidTree = new BinaryTree();
+    OrderLinkedList *example = new OrderLinkedList();
 
-    Limit oneL = Limit(25.00, 200, 200, NULL, NULL, NULL, NULL, NULL);
-    Order one = Order(1, true, 300, 25.50, NULL, NULL, NULL); // How do you insert a specific limit in here?
+    // Receive an Order
+    Order one = Order(1, true, 300, 25.50, NULL, NULL, NULL); 
+    
+    // Search correct Tree for limit
+    if (one.buyOrSell == true) {
+
+        //  If order Price Level exists in the Binary Tree
+        if(bidTree->isLimit(bidTree->getRoot(), one.orderPriceLevel)) {
+                        
+            //place Order at tail of the OrderLinkedList
+            example->addOrderFront(one.orderPriceLevel, &one);
+
+        } else {
+
+            // Insert new limit into tree at order price level
+            bidTree->insert(bidTree->getRoot(), one.orderPriceLevel);
+
+            // Create a linked List at this level
+            OrderLinkedList twentyFiveFifty = OrderLinkedList();
+            
+        } 
+
+    }
+
+    Limit oneL = Limit(25.00, 200, 200, NULL, NULL, NULL, NULL, NULL, NULL);
+    
     one.parentLimit = &oneL;
     
     bidTree->insert(bidTree->getRoot(), 43.43);
@@ -19,13 +44,15 @@ int main() {
     bidTree->insert(bidTree->getRoot(), 20.05);
 
 
+    
     std::cout << "Buy order book: \n";
     bidTree->inOrder(bidTree->getRoot());
     std::cout << "\n";
 
-    bool test = bidTree->isLimit(bidTree->getRoot(), 35); 
-    std::cout << test << "\n";
+    // Limit *test = Limit getLimit(bidTree->getRoot(), 35); 
+    //std::cout << test << "\n";
 
+    // delete test;
     delete bidTree;
 
     return 0;
